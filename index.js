faea6c316268e817d03347457d127a5930894723
arifal201 <arifalhidayat10@gmail.com>
2023-01-22 04:26:45 +0700

    try kill after running

--- index.js
+++ index.js
@@ -1,29 +1,60 @@
-var exec = require('child_process').execFile;
-var fun = function(){
+const exec = require('child_process').execFile;
+const command = exec("sleep 60");
+let openWorkApps = function(){
   exec("/mnt/c/Program Files/Docker/Docker/Docker Desktop.exe", function(err,data){
     console.log('open docker');
-    console.log(err);
+    if(err){
+      console.error(err);
+      return
+    }
     console.log(data.toString());
+    setTimeout(() => {
+      command.kill()
+    }, 1000)
   });
   exec("/mnt/c/Program Files (x86)/Google/Chrome/Application/chrome.exe", function(err,data){
     console.log('open chrome');
-    console.log(err);
+    if(err){
+      console.error(err);
+      return
+    }
     console.log(data.toString());
+    setTimeout(() => {
+      command.kill()
+    }, 1000)
   });
   exec("/mnt/c/Users/ASUS/AppData/Local/Postman/Postman.exe", function(err,data){
     console.log('open postman');
-    console.log(err);
+    if(err){
+      console.error(err);
+      return
+    }
     console.log(data.toString());
+    setTimeout(() => {
+      command.kill()
+    }, 1000)
   });
   exec("/mnt/c/Users/ASUS/AppData/Local/Programs/Microsoft VS Code/Code.exe", function(err,data){
     console.log('open vscode');
-    console.log(err);
+    if(err){
+      console.error(err);
+      return
+    }
     console.log(data.toString());
+    setTimeout(() => {
+      command.kill()
+    }, 1000)
   });
-    exec("/mnt/c/Users/ASUS/AppData/Local/Microsoft/WindowsApps/wt.exe", function(err,data){
-    console.log('open terminal');
-    console.log(err);
-    console.log(data.toString());
+  exec('ls', function(err, output){
+    console.log('try running ls');
+    if(err){
+      console.error(err);
+      return
+    }
+    console.log(output);
+    setTimeout(() => {
+      command.kill()
+    }, 1000)
   });
 }
-fun();
+openWorkApps();
